39b414d725133ce3e715cfe33787477169ed4687
gaoxiang <igaoxian@foxmail.com>
2021-01-20 17:27:43 +0800

    初体验

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>{{ count }},{{ double }}</div>
+    <button @click="add">点击</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref } from '@vue/composition-api'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+      const count = ref(1)
+      const double = computed(()=>{
+        return count.value*2
+      })
+     
+      const add = ()=>{
+        count.value++
+      }
+      return {count,double,add}
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Composition from '@vue/composition-api'
+Vue.use(Composition)
 Vue.config.productionTip = false
 
 new Vue({
